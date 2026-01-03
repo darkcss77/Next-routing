38704c5babe22d6aff934176b6d7937c4075b35b
Dark CSS <darkcss77@gmail.com>
2026-01-03 13:05:59 +0500

    Added Image Component

--- app/(users)/page.js
+++ app/(users)/page.js
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import ImageP from "@/public/image.jpg";
+
 export default function Home() {
-  return <h1>Hello world! This is Home Page</h1>;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl mx-auto mt-10 overflow-hidden relative">
+      <h3>Heading</h3>
+      <div className="image-card">
+        <Image
+          src={ImageP}
+          alt="Picture of the author"
+          fill
+          placeholder="blur"
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
 }
